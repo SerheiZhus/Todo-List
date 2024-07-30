635da01c02550ba290a89794c4dda6c34d5b67fc
sergey <38cerg12021979@jmal.com>
2024-07-30 15:58:46 +0300

    fix files black

--- task/models.py
+++ task/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.CharField(max_length=250)
+    created_task = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    boolean_field = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", related_name="tasks")
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
