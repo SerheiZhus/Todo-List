94d3d07389200fd7a96047382fa6ce70c2237f51
sergey <38cerg12021979@jmal.com>
2024-08-01 13:57:02 +0300

    create file forms.py

---
+++ task/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from task.models import Tag, Task
+
+
+class TaskForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+    deadline = forms.DateTimeField(
+        input_formats=["%Y-%m-%d %H:%M:%S"],
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"}),
+    )
+
+    class Meta:
+        model = Task
+        fields = ["content", "deadline", "boolean_field", "tags"]
